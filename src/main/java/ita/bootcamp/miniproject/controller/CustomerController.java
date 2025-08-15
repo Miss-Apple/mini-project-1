18ac627ad1ef16360c91c1016048d3d5b51217a2
PAUL JOSHUA BARREDO (CTS-LOSCP-ISD-OOCLL/MNL) <BARREPA@OOCL.COM>
2025-08-15 15:30:01 +0800

    CORS in controller

--- src/main/java/ita/bootcamp/miniproject/controller/CustomerController.java
+++ src/main/java/ita/bootcamp/miniproject/controller/CustomerController.java
@@ -4,14 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import ita.bootcamp.miniproject.model.Customer;
 import ita.bootcamp.miniproject.service.CustomerService;
@@ -21,6 +14,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/customer")
+@CrossOrigin(origins = "http://localhost:5177") // URL ng frontend
 public class CustomerController {
 
     private CustomerService customerService;

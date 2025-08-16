c8941fd5af89485a4e57d72fca97a630df6bd753
AQUINHI <hikarutaquino@gmail.com>
2025-08-16 19:28:32 +0800

    Initial security development for spring security

--- src/main/java/ita/bootcamp/miniproject/service/CustomerService.java
+++ src/main/java/ita/bootcamp/miniproject/service/CustomerService.java
@@ -2,6 +2,7 @@ package ita.bootcamp.miniproject.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ita.bootcamp.miniproject.model.Customer;
@@ -11,6 +12,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @Service
 public class CustomerService {
+
+    @Autowired
     private CustomerRepository customerRepository;
 
     // VIEW CUSTOMER

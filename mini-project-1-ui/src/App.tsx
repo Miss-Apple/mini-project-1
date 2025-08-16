37286073e541562300da77296968e8597c362681
annadese <annapat.desembrana@gmail.com>
2025-08-16 15:33:48 +0800

    Added findCustomerById in frontend to display queried customer details; Removed comments and fixed spacings

--- mini-project-1-ui/src/App.tsx
+++ mini-project-1-ui/src/App.tsx
@@ -1,44 +1,102 @@
-import { use, useEffect, useState } from "react";
-import { DataCard } from "./components/DataCard";
+import {useEffect, useState} from "react";
+import {DataCard} from "./components/DataCard";
 import "./App.css";
 import axios from "axios";
-import { Spin, Typography } from "antd";
-import { ErrorMessage } from "./components/ErrorMessage";
+import {Button, Form, Input, Spin, Typography} from "antd";
+import {ErrorMessage} from "./components/ErrorMessage";
+import CustomerModal from "./components/CustomerModal.tsx";
+
+
 
 function App() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const getAllCustomers = "api/customer/all";
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+    const [recordCount, setRecordCount] = useState(0);
+    const [customer, setCustomer] = useState(null);
+    const [openModal, setOpenModal] = useState(false);
+    const getAllCustomers = "api/customer/all";
+    const findCustomerCount = "api/customer/data/count";
 
-  useEffect(() => {
-    axios
-      .get(getAllCustomers)
-      .then((response) => {
-        setData(response.data);
-        setIsLoading(false);
-        console.log("SUCCESS");
-      })
-      .catch((err) => {
-        setIsError(err.message);
-        setIsLoading(false);
-        console.log("ERROR");
-      });
-  }, []);
+    const [form] = Form.useForm();
 
-  return (
-    <>
+    const onFinish = (value: { inputCusId: string; }) => {
+        const findCustomerById = "api/customer/" + value.inputCusId;
+        axios
+            .get(findCustomerById)
+            .then((response) => {
+                setCustomer(response.data);
+                setIsLoading(false);
+                setOpenModal(true)
+                console.log("SUCCESS")
+                console.log("openModal: " + open);
+            })
+            .catch((err) => {
+                setIsError(err.message);
+                setIsLoading(false);
+                console.log("ERROR");
+            });
+        console.log(customer);
+    };
 
-      <Spin spinning={isLoading} tip="Loading...">
-        {!isError && <DataCard customers={data} />}
-        {isError && (
-          <Typography.Paragraph>
-            <ErrorMessage openModal={isError} />
-          </Typography.Paragraph>
-        )}
-      </Spin>
-    </>
-  );
+    useEffect(() => {
+        axios
+            .get(getAllCustomers)
+            .then((response) => {
+                setData(response.data);
+                setIsLoading(false);
+                console.log("SUCCESS");
+            })
+            .catch((err) => {
+                setIsError(err.message);
+                setIsLoading(false);
+                console.log("ERROR");
+            });
+        axios
+            .get(findCustomerCount)
+            .then((response) => {
+                setRecordCount(response.data);
+                setIsLoading(false);
+                console.log("SUCCESS");
+            })
+            .catch((err) => {
+                setIsError(err.message);
+                setIsLoading(false);
+                console.log("ERROR");
+            });
+    }, []);
+
+    return (
+        <>
+            <Spin spinning={isLoading} tip="Loading...">
+                {!isError && <DataCard customers={data}/>}
+                {isError && (
+                    <Typography.Paragraph>
+                        <ErrorMessage openModal={isError}/>
+                    </Typography.Paragraph>
+                )}
+            </Spin>
+            <Form
+                form={form}
+                name="control-hooks"
+                onFinish={onFinish}
+                style={{ maxWidth: 600 }}>
+                <Form.Item name="inputCusId" label="Find Customer">
+                    <Input />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+            <Typography.Text>{recordCount}</Typography.Text>
+            {openModal &&
+                <CustomerModal openModal={openModal} customer={customer}/>
+            }
+        </>
+
+    );
 }
 
 export default App;
--- mini-project-1-ui/src/components/DataCard.tsx
+++ mini-project-1-ui/src/components/DataCard.tsx
@@ -1,33 +1,29 @@
-import React from "react";
-import { Card, Col, Row, Spin } from "antd";
+import {Card} from "antd";
 
-// type DataCardProps = {
-//  data: number[];
-// };
 type CustomerProps = {
-  customerId: number;
-  firstName: string;
-  lastName: string;
-  contactNumber: string;
-  emailAddress: string;
-  addressLine1: string;
-  addressLine2: string;
-  locationId: number;
+    customerId: number;
+    firstName: string;
+    lastName: string;
+    contactNumber: string;
+    emailAddress: string;
+    addressLine1: string;
+    addressLine2: string;
+    locationId: number;
 };
 
 type DataCardProps = {
-  customers: CustomerProps[];
+    customers: CustomerProps[];
 };
 
-export const DataCard = ({ customers }: DataCardProps) => (
-      <Card title="Card title" variant="borderless">
-        
-          {customers.map((customer) => (
+export const DataCard = ({customers}: DataCardProps) => (
+    <Card title="Card title" variant="borderless">
+
+        {customers.map((customer) => (
             <div key={customer.customerId}>
-              {customer.firstName} {customer.lastName}
+                {customer.firstName} {customer.lastName}
             </div>
-          ))}
-        
-      </Card>
+        ))}
+
+    </Card>
 );
 
